aa54e7e032fb1259df958f82ce409c43ac277b80
Uicab04 <72843263+Uicab04@users.noreply.github.com>
2025-07-07 10:55:01 -0500

    Update link mapa Contact.js
    cambie el link del mapa

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -18,7 +18,7 @@
         <h3 className="map-title">Ubicación</h3>
         <iframe
           title="Mapa de Cancun riviera maya"
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3762.6937891411504!2d-87.0738856845405!3d20.62895658617962!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8f4c2a2c5e5c9e0d%3A0x2e7e4c5d8f1b2e0!2sPlaya%20del%20Carmen%2C%20Quintana%20Roo%2C%20Mexico!5e0!3m2!1sen!2sus!4v1634657891234!5m2!1sen!2sus"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3723.524726725977!2d-86.84750338506244!3d21.161908888995695!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8f4c2be3e7e78dab%3A0x9e6d5c9f4e5e2e4e!2sCanc%C3%BAn%2C%20Quintana%20Roo%2C%20M%C3%A9xico!5e0!3m2!1ses-419!2smx!4v1720367100000!5m2!1ses-419!2smx"
           width="300"
           height="250"
           style={{ border: 0 }}
